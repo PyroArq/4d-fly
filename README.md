27c6851db1c147ebf2dd8a0e46dbea8aeca9204f
Redjard <github@redjard.org>
2023-11-21 06:28:47 +0100

    initial upload of v2.7

---
+++ README.md
@@ -0,0 +1,16 @@
+# 4D SmoothFly
+
+A mod for [4D Miner by Mashpoe](https://mashpoe.com/4d-miner), based on [Tr1NgleDev's 4dm.h modding framework](https://github.com/4D-Modding/4dm.h).
+
+This mods is an improvement on Tr1Ngles 4DFly mod, improving flight in numerous ways.
+
+- Gravity is fully disabled in all edge cases
+- your velocity does not jump unpredictably when you fly into walls or the floor
+- flight is more snappy
+- flight in singleplayer is way faster
+- equal feel in all 4 axes
+- more stable acceleration and deceleration
+- if you fly off a block horizontally, you can fly back on
+
+
+The Hotkey for flight is F, this mod is intended for use with the 4DKeyBinds mod, but does not require it.
---
+++ main.cpp
@@ -0,0 +1,99 @@
+
+#define MOD_NAME "4D SmoothFly"
+#define MOD_VER "2.7"
+
+#include <4dm.h>
+#include "4DKeyBinds.h"
+using namespace fdm;
+
+bool fly = false;
+
+// movement rewrite
+double epsilon = 0.0001;
+void(__thiscall* Player_updatePos)(Player* self, World* world, double dt);
+void Player_updatePos_H(Player* self, World* world, double dt) {
+	
+	if (!fly)
+		return Player_updatePos(self, world, dt);
+	
+	// if we are suspiciously slightly below a full block, move us up on it a tad
+	if ( floor(self->pos.y + epsilon) > floor(self->pos.y - epsilon) )
+		self->pos.y += 2*epsilon;
+	
+	float speed = 15;
+	if (self->keys.ctrl)
+		speed = 30;
+	if (self->inventoryManager.secondary)  // inventory open
+		speed = 0;
+	
+	// censorship courtesy Tr1Ngle
+	if (world->getType()==World::TYPE_CLIENT)
+		speed /= 2;
+	
+	float slide = 0.1;  // natural decay time towards final speed, every this much we get e times closer
+	
+	// calculate forward in the horizontal plane, self-forward is down if we look down, not what we want
+	glm::vec4 up = {0,1,0,0};
+	glm::vec4 forward = m4::cross(self->left, up, self->over);
+	
+	glm::vec4 direction(0);
+	
+	// each movement key adds its own component, we normalize this out later
+	if (self->keys.space)
+		direction += up;
+	if (self->keys.shift)
+		direction -= up;
+	if (self->keys.w)
+		direction += forward;
+	if (self->keys.s)
+		direction -= forward;
+	if (self->keys.a)
+		direction += self->left;
+	if (self->keys.d)
+		direction -= self->left;
+	if (self->keys.q)
+		direction += self->over;
+	if (self->keys.e)
+		direction -= self->over;
+	
+	// normalize, but leave at 0 if no direction   the max(1,...) prevents division by 0
+	direction = direction / std::max( 1.f, glm::length(direction) );
+	
+	glm::vec4 targetVel = direction*speed;
+	
+	self->deltaVel = ( targetVel - self->vel ) * float(dt)/slide;
+	
+	// let game do movement calculation properly, can't do much here since this handles collision
+	// we could change self->vel, this was done originally, but self->deltaVel is sufficient for us
+	self->crouching = false;
+	self->keepOnEdge = false;
+	
+	return Player_updatePos(self, world, dt);
+}
+
+void toggleFly(){
+	
+	fly = !fly;
+	
+	if (fly)
+		Player_updatePos_H(&StateGame::instanceObj->player,&*StateGame::instanceObj->world,0);
+	
+	printf("fly: %s\n", fly ? "on" : "off");
+}
+
+
+DWORD WINAPI Main_Thread(void* hModule) {
+	
+	KeyBinds::addBind( "Toggle Flight", glfw::Keys::F, KeyBindsScope::PLAYER, [](GLFWwindow* window, int action, int mods){ if (action == GLFW_PRESS) toggleFly(); });
+	
+	Hook( FUNC_PLAYER_UPDATEPOS, Player_updatePos_H, &Player_updatePos );
+	
+	EnableHook();
+	return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD _reason, LPVOID lpReserved) {
+	if (_reason == DLL_PROCESS_ATTACH)
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Main_Thread, hModule, 0, NULL);
+	return true;
+}
